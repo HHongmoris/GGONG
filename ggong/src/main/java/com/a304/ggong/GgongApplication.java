453c11e4d9ac96737feaa6ddc350a98365f7451d
정예지 <wjddpwl1222@gmail.com>
2023-07-24 17:47:07 +0900

    add file

---
+++ ggong/src/main/java/com/a304/ggong/GgongApplication.java
@@ -0,0 +1,13 @@
+package com.a304.ggong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgongApplication.class, args);
+	}
+
+}
---
+++ ggong/src/test/java/com/a304/ggong/GgongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a304.ggong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
