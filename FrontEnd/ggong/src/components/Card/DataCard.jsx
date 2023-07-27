fb03dde76178633ff776ad457b46f97affadf995
bmincof <bminlvcoffee@gmail.com>
2023-07-27 12:39:49 +0900

    Style: Change style of DataCard

--- FrontEnd/ggong/src/components/Card/DataCard.jsx
+++ FrontEnd/ggong/src/components/Card/DataCard.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 
+import { Background, Border } from '../../global/colors';
+
+import { Subtitle } from '../Heading';
+
 // 데이터 카드 컴포넌트 : 제목(어떤 데이터인지), 데이터를 항목으로 갖는다.
 const DataCard = ({ title = '제목', data = '데이터' }) => {
+  const bgTitle = Background.MAIN;
+  const bgContent = Background.WHITE;
+  const borderColor = Border.MAIN;
   // title과 data에 전달된 값이 없으면 제목과 데이터가 출력
   return (
     // DOM
     // card라는 클래스에 너비-32 bg-base-100 등의 속성 넣어줌. card-body에서 아이템들 가운데 정렬 및 배경색 노란색으로 변경
     // style에서는 padding-card 속성을 1rem으로 바꿔서 설정 (기본값이 2rem이었음)
-    <div className="card md: w-2/5 bg-base-100 shadow-xl rounded-md border border-zinc-400">
+    <div className={`card md: w-36 ${bgContent} shadow-xl rounded-md border ${borderColor}`}>
       <div
-        className="card-body items-center md: h-1/5 bg-yellow-400 border border-zinc-400"
+        className={`card-body items-center md: h-1/5 ${bgTitle} border ${borderColor}`}
         style={{ '--padding-card': '1rem' }}
       >
-        <h2 className="card-title rounded-2xl">{title}</h2>
+        <Subtitle content={title} />
+        {/* <span className="font-bold">{title}</span> */}
       </div>
-      <div className="card-body md: h-1/5 items-center border border-zinc-400" style={{ '--padding-card': '1rem' }}>
+      <div className={`card-body md: h-1/5 items-center border ${borderColor}`} style={{ '--padding-card': '1rem' }}>
         <p>{data}</p>
+        {/* <p className="font-bold">{data}</p> */}
+        {/* <Subtitle content={data + ' 명'} /> */}
       </div>
     </div>
   );
