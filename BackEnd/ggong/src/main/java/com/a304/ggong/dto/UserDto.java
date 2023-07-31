c10ef4bb6fd4ec61ed738e0c80cdcdcacddcbb0d
전주영 <bnb3228@naver.com>
2023-07-31 15:54:58 +0900

    Feat: Add UserDto.java & User.java

---
+++ BackEnd/ggong/src/main/java/com/a304/ggong/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.a304.ggong.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserDto {
+
+    private int user_no;
+    private String name;
+    private String age_range;
+    private String gender;
+    private String email;
+    private String favorite_cigarette;
+    private String QR;
+    private String user_rating;
+}
