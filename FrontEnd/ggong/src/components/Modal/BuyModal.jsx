659152b5db3748bf315ebc4455f5999e90d008f5
bmincof <bminlvcoffee@gmail.com>
2023-07-31 10:43:54 +0900

    Comment: Add Comments to Modal

--- FrontEnd/ggong/src/components/Modal/BuyModal.jsx
+++ FrontEnd/ggong/src/components/Modal/BuyModal.jsx
@@ -3,16 +3,25 @@ import React, { useState } from 'react';
 import Button from '../Button/Button';
 import ModalContent from './BuyModalContent';
 
+/**
+ * 구매 모달 윈도우 컴포넌트를 반환하는 함수 <br>
+ * 구매 버튼을 클릭하면 모달창이 열리고 확인, 취소 버튼을 선택할 수 있음 <br>
+ *
+ * @returns 구매 모달 컴포넌트
+ */
 const BuyModal = () => {
+  // 모달창이 보이는 상태를 저장하는 변수
   const [visible, setVisible] = useState(false);
 
+  // visible의 상태를 토글하는 함수
   const toggleVisible = () => {
     setVisible(!visible);
   };
 
   return (
     <div>
-      <Button value={'구매'} handleClick={toggleVisible} />
+      <Button value={'구매'} handleClick={toggleVisible} size="small" />
+      {/* 아래는 모달창 컴포넌트 */}
       <ModalContent open={visible} toggleVisible={toggleVisible} />
     </div>
   );
--- FrontEnd/ggong/src/components/Modal/BuyModalContent.jsx
+++ FrontEnd/ggong/src/components/Modal/BuyModalContent.jsx
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 import { Subtitle } from '../Heading';
 import Button from '../Button/Button';
 
+/**
+ * 모달창의 내용 컴포넌트를 반환하는 함수
+ *
+ * @param {boolean} open 현재 컴포넌트가 보이는지 여부
+ * @param {Function} toggleVisible open의 상태를 변경하는 함수
+ * @returns 모달창 내용 컴포넌트
+ */
 const ModalContent = ({ open, toggleVisible }) => {
   // TODO: 상품 구매로직 추가하기
   const buyProduct = () => {};
@@ -10,9 +17,9 @@ const ModalContent = ({ open, toggleVisible }) => {
     <div>
       {open && (
         <div className="absolute left-0 top-0 w-full h-full flex justify-center items-center">
-          <div className="absolute left-0 top-0 w-full h-full opacity-50 bg-gray-400"></div>
+          <div className="absolute left-0 top-0 w-full h-full opacity-50 bg-gray-400 z-20"></div>
           {/* 구매 안내문 영역 */}
-          <div className="card w-80 bg-white">
+          <div className="card w-80 bg-white z-20">
             <div className="card-body">
               {/* 안내문 */}
               <Subtitle content="상품권 구매" />
