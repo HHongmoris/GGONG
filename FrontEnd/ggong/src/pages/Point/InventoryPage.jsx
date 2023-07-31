d68c88d16c33b336e90e626d6d7ef9590614725e
정의석 <euiseok96@gmail.com>
2023-07-31 16:22:27 +0900

    Feat: Create InventoryPage

--- FrontEnd/ggong/src/pages/Point/InventoryPage.jsx
+++ FrontEnd/ggong/src/pages/Point/InventoryPage.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import InventoryList from '../../components/List/InventoryList';
+
+const inventoryData = [
+  {
+    price: 5000,
+    PIN: 'ABCD-EFGH-ABCD-EFGH',
+  },
+  {
+    price: 50000,
+    PIN: '1234-5678-1234-5678',
+  },
+  {
+    price: 10000,
+    PIN: '1234-EFGH-1234-EFGH',
+  },
+  {
+    price: 5000,
+    PIN: 'ABCD-EFGH-ABCD-EFGH',
+  },
+  {
+    price: 50000,
+    PIN: 'ABCD-EFGH-ABCD-EFGH',
+  },
+  {
+    price: 10000,
+    PIN: 'ABCD-EFGH-ABCD-EFGH',
+  },
+];
 
 // 보관함 페이지
 const InventoryPage = () => {
-  return <></>;
+  return (
+    <div className="mt-12">
+      <InventoryList products={inventoryData} />
+    </div>
+  );
 };
 
 export default InventoryPage;
