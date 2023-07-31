4ecb3f711d408580b8145229ab4b2f3e6c454178
정의석 <euiseok96@gmail.com>
2023-07-31 13:35:07 +0900

    Feat: Create PointPageLayout

--- FrontEnd/ggong/src/pages/Point/PointPageLayout.jsx
+++ FrontEnd/ggong/src/pages/Point/PointPageLayout.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import ProfileCard from '../../components/Card/ProfileCard';
 
 // 포인트 관련 페이지에 공통으로 적용되는 레이아웃
 const PointPageLayout = () => {
-  return <></>;
+  return <ProfileCard></ProfileCard>;
 };
 
 export default PointPageLayout;
