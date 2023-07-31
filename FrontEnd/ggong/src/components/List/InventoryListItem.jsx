ab681ea63b279c3d86e72015b9306aa4d02d102c
정의석 <euiseok96@gmail.com>
2023-07-31 16:33:59 +0900

    Refactor: Add Comments

--- FrontEnd/ggong/src/components/List/InventoryListItem.jsx
+++ FrontEnd/ggong/src/components/List/InventoryListItem.jsx
@@ -4,15 +4,13 @@ import { Text } from '../../global/colors';
 import Coupon from './Coupon';
 
 /**
- * 포인트 내역 리스트에서 한 내역을 표시하기 위한 컴포넌트
+ * 구매내역 리스트에서 한 내역을 표시하기 위한 컴포넌트
  *
- * @param {number} variant 포인트의 변동량을 담고 있는 정수
- * @param {number} balance 잔여 포인트를 담고 있는 정수
+ * @param {number} price 문화상품권의 가격을 담고 있는 정수
+ * @param {String} PIN 구매한 문화상품권의 PIN번호를 담고 있는 문자열
  * @returns 전달받은 값들로 구성된 하나의 내역 컴포넌트를 반환
  */
 const InventoryListItem = ({ price = 0, PIN = 'AAAA-AAAA-AAAA-AAAA' }) => {
-  const priceSep = price.toLocaleString('ko-KR');
-
   return (
     <div>
       <div className="flex justify-between mb-8 mt-8">
