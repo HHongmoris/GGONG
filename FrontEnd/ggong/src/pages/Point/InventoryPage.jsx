7ab26915d3a74be06ea4637df2b5f8923956b4ce
정의석 <euiseok96@gmail.com>
2023-07-31 16:34:20 +0900

    Refactor: Add Commments

--- FrontEnd/ggong/src/pages/Point/InventoryPage.jsx
+++ FrontEnd/ggong/src/pages/Point/InventoryPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import InventoryList from '../../components/List/InventoryList';
 
+// 구매한 상품 가상데이터
 const inventoryData = [
   {
     price: 5000,
@@ -28,7 +29,7 @@ const inventoryData = [
   },
 ];
 
-// 보관함 페이지
+// 상품보관함 페이지
 const InventoryPage = () => {
   return (
     <div className="mt-12">
