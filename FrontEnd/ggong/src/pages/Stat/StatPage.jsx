b7efad36c6cd8ab1d9e64e82bd584a8a3d675b4d
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-01 15:50:49 +0900

    Feat: Create StatPage

--- FrontEnd/ggong/src/pages/Stat/StatPage.jsx
+++ FrontEnd/ggong/src/pages/Stat/StatPage.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 
+import DataCard from '../../components/Card/DataCard';
+import { Title } from '../../components/Heading';
+import PieChart from '../../components/Chart/PieChart';
+import Table from '../../components/Table/Table';
+
 // 통계 페이지
-const StatPage = () => {
-  return <></>;
+const StatPage = ({ today, users = [], pieUsers = [], machineUsers = [] }) => {
+  const [userAll, userLastMonth] = users;
+  const [userAge, userGender] = pieUsers;
+
+  return (
+    <div>
+      <div className="flex justify-between mb-8">
+        <DataCard title="금일 이용자" data={today} />
+        <DataCard title="전체 회원 수" data={userAll} />
+      </div>
+      <div className="flex justify-between mb-8">
+        <DataCard title="지난달 총 사용자" data={userLastMonth} />
+        <DataCard title="지난달 평균 사용자" data={Math.round(userLastMonth / 30)} />
+      </div>
+      <div className="mb-8">
+        <Title content="사용자 수" spacing={true} />
+        <div className="flex justify-between">
+          <PieChart title="연령별" data={userAge} />
+          <PieChart title="성별" data={userGender} />
+        </div>
+      </div>
+      <div>
+        <Table data={machineUsers} />
+      </div>
+    </div>
+  );
 };
 
 export default StatPage;
