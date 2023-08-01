fe20c6e71a1406e5f007117b5d95a8a9c127cbeb
정예지 <wjddpwl1222@gmail.com>
2023-08-01 16:34:03 +0900

    Feat: Mod Entitiy of FavoriteMachine, Machine, Question, User, Vote

--- BackEnd/ggong/src/main/java/com/a304/ggong/entity/FavoriteMachine.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/entity/FavoriteMachine.java
@@ -9,13 +9,17 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "favoritemachine")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class FavoriteMachine {
 
 	@Id
--- BackEnd/ggong/src/main/java/com/a304/ggong/entity/Question.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/entity/Question.java
@@ -8,13 +8,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "question")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Question {
 
 	@Id
