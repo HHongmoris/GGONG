ca7563cd1add2409990d073b76ce6e800e76ab08
HHongmoris <moris8470@gmail.com>
2023-08-01 17:29:49 +0900

    Feat: Add AllItemResponse

--- BackEnd/ggong/src/main/java/com/a304/ggong/dto/response/AllItemResponse.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/dto/response/AllItemResponse.java
@@ -1,6 +1,21 @@
 package com.a304.ggong.dto.response;
 
+import com.a304.ggong.entity.Product;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AllItemResponse {
+    private long productNo;
+    private int price;
 
-
+//    //얘는 리스폰스니까 이거 없어야함
+//    public Product toEntity(){
+//        return Product.builder()
+//                .productNo(productNo)
+//                .price(price)
+//                .build();
+//    }
 }
