304745549c8deb7d5e092f0322e69268d8ed5349
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-02 17:29:24 +0900

    Feat: Create Point pages

--- FrontEnd/ggong/src/pages/Point/InventoryContainer.jsx
+++ FrontEnd/ggong/src/pages/Point/InventoryContainer.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import InventoryPage from './InventoryPage';
 // 보관함 페이지에 필요한 데이터를 관리하는 컨테이너
 const InventoryContainer = () => {
-  return <></>;
+  return (
+    <div>
+      <InventoryPage />
+    </div>
+  );
 };
 
 export default InventoryContainer;
--- FrontEnd/ggong/src/pages/Point/PointHistoryContainer.jsx
+++ FrontEnd/ggong/src/pages/Point/PointHistoryContainer.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import PointHistoryPage from './PointHistoryPage';
 // 포인트 내역 페이지에 필요한 데이터를 관리하는 컨테이너
 const PointContainer = () => {
-  return <></>;
+  return (
+    <div>
+      <PointHistoryPage />
+    </div>
+  );
 };
 
 export default PointContainer;
