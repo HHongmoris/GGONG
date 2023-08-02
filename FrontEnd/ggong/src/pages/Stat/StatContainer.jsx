f98d52bac89c313c3278158b6b955649b08ca117
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-02 17:29:50 +0900

    Feat: Create Stat page

--- FrontEnd/ggong/src/pages/Stat/StatContainer.jsx
+++ FrontEnd/ggong/src/pages/Stat/StatContainer.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import StatPage from './StatPage';
 // 통계 페이지에 필요한 데이터를 관리하는 컨테이너
 const StatContainer = () => {
-  return <></>;
+  return (
+    <div>
+      <StatPage />
+    </div>
+  );
 };
 
 export default StatContainer;
