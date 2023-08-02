10821b63499430033553fb9d233e49c284eb3c84
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-02 17:30:12 +0900

    Feat: Create Answer pages

--- FrontEnd/ggong/src/pages/Answer/AnswerContainer.jsx
+++ FrontEnd/ggong/src/pages/Answer/AnswerContainer.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 // 질문 응답 페이지에서 사용할 데이터를 관리하는 컨테이너
-const AnswerContainerPage = () => {
-  return <></>;
+const AnswerContainer = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
-export default AnswerContainerPage;
+export default AnswerContainer;
