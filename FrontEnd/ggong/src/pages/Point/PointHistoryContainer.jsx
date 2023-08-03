454ee83f0a8b99a270510053ddfbc9849fc7c766
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-03 12:42:33 +0900

    Feat: Add data managing

--- FrontEnd/ggong/src/pages/Point/PointHistoryContainer.jsx
+++ FrontEnd/ggong/src/pages/Point/PointHistoryContainer.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import PointHistoryPage from './PointHistoryPage';
+import useApi from '../../hooks/useApi';
 // 포인트 내역 페이지에 필요한 데이터를 관리하는 컨테이너
 const PointContainer = () => {
+  const [points, setPoints] = useState([]);
+
+  useEffect(() => {
+    useApi('/points/', 'GET').then(res => {
+      setPoints(res.data);
+    });
+  }, []);
+
   return (
     <div>
-      <PointHistoryPage />
+      <PointHistoryPage data={points} />
     </div>
   );
 };
