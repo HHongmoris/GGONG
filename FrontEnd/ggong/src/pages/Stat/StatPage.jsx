9d9c11441e27ef5074aa66d1f247a1c7ae50fa78
정의석 <euiseok96@gmail.com>
2023-08-03 14:02:30 +0900

    Fix: Redefine variable for users, pieAges and pieGender

--- FrontEnd/ggong/src/pages/Stat/StatPage.jsx
+++ FrontEnd/ggong/src/pages/Stat/StatPage.jsx
@@ -14,28 +14,27 @@ import Table from '../../components/Table/Table';
  * @param {Data[]} machineUsers 기기별 사용자 수 테이블에 들어갈 데이터
  * @returns 통계 페이지
  */
-const StatPage = ({ today, users = [], pieUsers = [], machineUsers = [] }) => {
+const StatPage = ({ today, users = [], pieAges, pieGender, machineUsers = [] }) => {
   // 배열로 전달받은 users와 pieUsers props를 구조분해할당 합니다.
-  const [userAll, userLastMonth] = users;
-  const [userAge, userGender] = pieUsers;
+  const { totalUser, lastMonthUser } = users;
 
   return (
     <div>
       {/* 데이터 카드 영역 */}
       <div className="flex justify-between mb-8">
         <DataCard title="금일 이용자" data={today} />
-        <DataCard title="전체 회원 수" data={userAll} />
+        <DataCard title="전체 회원 수" data={totalUser} />
       </div>
       <div className="flex justify-between mb-8">
-        <DataCard title="지난달 총 사용자" data={userLastMonth} />
-        <DataCard title="지난달 평균 사용자" data={Math.round(userLastMonth / 30)} />
+        <DataCard title="지난달 총 사용자" data={lastMonthUser} />
+        <DataCard title="지난달 평균 사용자" data={Math.round(lastMonthUser / 30)} />
       </div>
       {/* 파이 차트 영역 */}
       <div className="mb-8">
         <Title content="사용자 수" spacing={true} />
         <div className="flex justify-between">
-          <PieChart title="연령별" data={userAge} />
-          <PieChart title="성별" data={userGender} />
+          <PieChart title="연령별" data={pieAges} />
+          <PieChart title="성별" data={pieGender} />
         </div>
       </div>
       {/* 테이블 영역 */}
