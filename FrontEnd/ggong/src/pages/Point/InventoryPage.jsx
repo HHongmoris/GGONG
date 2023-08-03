2dcded540730bce7af66a4c67da2bbe881b05913
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-03 12:56:15 +0900

    Chore: Remove unused variables

--- FrontEnd/ggong/src/pages/Point/InventoryPage.jsx
+++ FrontEnd/ggong/src/pages/Point/InventoryPage.jsx
@@ -1,34 +1,6 @@
 import React from 'react';
 import InventoryList from '../../components/List/InventoryList';
 
-// 구매한 상품 가상데이터
-const inventoryData = [
-  {
-    price: 5000,
-    PIN: 'ABCD-EFGH-ABCD-EFGH',
-  },
-  {
-    price: 50000,
-    PIN: '1234-5678-1234-5678',
-  },
-  {
-    price: 10000,
-    PIN: '1234-EFGH-1234-EFGH',
-  },
-  {
-    price: 5000,
-    PIN: 'ABCD-EFGH-ABCD-EFGH',
-  },
-  {
-    price: 50000,
-    PIN: 'ABCD-EFGH-ABCD-EFGH',
-  },
-  {
-    price: 10000,
-    PIN: 'ABCD-EFGH-ABCD-EFGH',
-  },
-];
-
 // 상품보관함 페이지
 const InventoryPage = ({ products = [] }) => {
   return (
