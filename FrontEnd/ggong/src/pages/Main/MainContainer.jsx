dab26c70fa6541cafdf532a7fd34b8f0d0e08f35
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-03 12:37:42 +0900

    Feat: Add data managing

--- FrontEnd/ggong/src/pages/Main/MainContainer.jsx
+++ FrontEnd/ggong/src/pages/Main/MainContainer.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MainPage from './MainPage';
+
+import useApi from '../../hooks/useApi';
 // 메인 페이지에 필요한 데이터를 관리하는 컨테이너
 const MainContainer = () => {
+  const [user, setUser] = useState({});
+  const [today, setToday] = useState(0);
+  const [yesterday, setYesterday] = useState(0);
+  const [likes, setLikes] = useState([]);
+
+  useEffect(() => {
+    useApi('/users/1', 'GET').then(res => {
+      setUser(res.data[0], console.log(user));
+    });
+
+    useApi('/users/smoke', 'GET').then(res => {
+      setToday(res.data.currentCount);
+      setYesterday(res.data.pastCount);
+    });
+
+    useApi('/users/like', 'GET').then(res => {
+      setLikes(res.data);
+    });
+  }, []);
+
   return (
     <div>
-      <MainPage />
+      <MainPage user={user} today={today} yesterday={yesterday} machines={likes} />
     </div>
   );
 };
--- FrontEnd/ggong/src/pages/Main/MainPage.jsx
+++ FrontEnd/ggong/src/pages/Main/MainPage.jsx
@@ -5,19 +5,20 @@ import DataCard from '../../components/Card/DataCard';
 import MachineCard from '../../components/Card/MachineCard';
 
 // 메인 페이지
-const MainPage = ({ user = {}, today = 0, yesterday = 0 }) => {
-  const { grade = '', nickname = 'dsa', point = 0, qr = '' } = user;
+const MainPage = ({ user = {}, today = 0, yesterday = 0, machines = [] }) => {
+  const { userRating = '', nickname = '', points = 0, QR = '' } = user;
+  // console.log('메인페이지', machines[0].question);
 
   return (
     <div>
-      <ProfileCard grade={grade} nickname={nickname} point={point} qr={qr} />
+      <ProfileCard grade={userRating} nickname={nickname} point={points} qr={QR} />
       <div className="flex justify-between my-20">
         {/* 오늘 담배 수 */}
         <DataCard title="오늘 투표수" data={today} />
         {/* 어제 담배 수 */}
         <DataCard title="어제 투표수" data={yesterday} />
       </div>
-      {/* <MachineCard /> */}
+      <MachineCard machines={machines} />
     </div>
   );
 };
