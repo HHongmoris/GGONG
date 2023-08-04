991640f4763143a12eaa4114484cd2f032e91264
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-04 15:43:17 +0900

    Chore: Remove unused vars

--- FrontEnd/ggong/src/pages/Login/LoginPage.jsx
+++ FrontEnd/ggong/src/pages/Login/LoginPage.jsx
@@ -9,15 +9,11 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 
 import Button from '../../components/Button/Button';
-import { useLocation } from 'react-router-dom';
 
 // 로그인 페이지
 const LoginPage = () => {
   // TODO : 로그인 버튼에 로그인 API 연결하기
   let dispatch = useDispatch();
-
-  const location = useLocation();
-  console.log(location.pathname);
 
   const onClickLogin = () => {
     useApi('/users/1', 'GET').then(res => {
