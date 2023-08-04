428d77975146ca0c8633ab26f07641b16ea4b8f8
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-04 14:45:02 +0900

    Feat: Create temporal login function

--- FrontEnd/ggong/src/pages/Login/LoginPage.jsx
+++ FrontEnd/ggong/src/pages/Login/LoginPage.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-
 import Logo from '../../assets/logo.png';
 import LoginButton from '../../assets/kakao_login_medium_wide.png';
 
 import { Background } from '../../global/colors';
+import { login } from '../../global/store';
+import useApi from '../../hooks/useApi';
+
+import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import Button from '../../components/Button/Button';
+import { useLocation } from 'react-router-dom';
 
 // 로그인 페이지
 const LoginPage = () => {
   // TODO : 로그인 버튼에 로그인 API 연결하기
+  let dispatch = useDispatch();
+
+  const location = useLocation();
+  console.log(location.pathname);
+
+  const onClickLogin = () => {
+    useApi('/users/1', 'GET').then(res => {
+      let response = res.data[0];
+      console.log(response);
+      dispatch(login(response));
+    });
+  };
 
   return (
     <div className={`${Background.MAIN} flex flex-col justify-center items-center h-screen space-y-40`}>
       <img src={Logo} className="w-3/4" />
-      <Button value={<img src={LoginButton} />} />
+      <Button value={<img src={LoginButton} onClick={onClickLogin} />} />
     </div>
   );
 };
