4f80c6af1a65606c0dbf835c5a39ac05c4973e3d
정예지 <wjddpwl1222@gmail.com>
2023-08-04 17:53:02 +0900

    Feat: Mod MachineRepository, QuestionRepository, VoteRepository, AnswerService

--- BackEnd/ggong/src/main/java/com/a304/ggong/service/AnswerService.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/service/AnswerService.java
@@ -3,23 +3,12 @@ package com.a304.ggong.service;
 import java.util.List;
 
 import com.a304.ggong.dto.response.AllAnswerResponse;
-import com.a304.ggong.dto.response.AnswerDetailResponse;
 
 public interface AnswerService {
 
 	// service단에서 각각의 resDto에 answerA, answerB와 rateA, rateB의 값을 넣어줘야함.
 
-	// 지역별 질문 응답 데이터 조회
-	public List<AnswerDetailResponse> getAnswersGroupByArea();
-
-	// 연령별 질문 응답 데이터 조회
-	public List<AnswerDetailResponse> getAnswersGroupByAge();
-
-	// 대학별 질문 응답 데이터 조회
-	public List<AnswerDetailResponse> getAnswersGroupByUni();
-
-	// 회사별 질문 응답 데이터 조회
-	public List<AnswerDetailResponse> getAnswersGroupByCompany();
+	// 질문 상세 페이지
 
 	// 공통 질문 응답 데이터 조회
 	public List<AllAnswerResponse> getAnswersGroupByCommon();
