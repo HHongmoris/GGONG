881b816c8ae405d1ee10e0a50c28689b370deb20
정예지 <wjddpwl1222@gmail.com>
2023-08-04 12:18:44 +0900

    Feat: Mod AllAnswerResponse, VoteRepository, AnswerService | Add AnswerDetailResponse

--- BackEnd/ggong/src/main/java/com/a304/ggong/repository/VoteRepository.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/repository/VoteRepository.java
@@ -4,34 +4,36 @@ import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
-import com.a304.ggong.dto.response.AllAnswerResponse;
-import com.a304.ggong.dto.response.AreaAnswerResponse;
-import com.a304.ggong.entity.Machine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.a304.ggong.dto.response.AllAnswerResponse;
+import com.a304.ggong.entity.Machine;
 import com.a304.ggong.entity.Vote;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
+	// return값이 Vote이므로 service에서 entity -> dto로 넘겨주는 로직 필요!
 	Optional<Vote> findByUserId(Long userId);
 
 	Optional<Vote> findByMachineId(Long machineId);
 
 	// group별(지난주 or 이번주) 모든 질문 리스트 조회
-	@Query("SELECT new com.a304.ggong.dto.response.AllAnswerResponse(q) FROM Question q WHERE q.type = '공통'")
-	List<AllAnswerResponse> findByQuestionGroup(int questionGroup);
+	// type을
+	@Query("SELECT new com.a304.ggong.dto.response.AllAnswerResponse(q) FROM Question q WHERE q.type = :type GROUP BY q.group HAVING q.group = :questionGroup")
+	List<AllAnswerResponse> findByQuestionGroup(@Param("questionGroup") int questionGroup, @Param("type") String type);
 
 	// 얘는 프론트랑 상의 후에...(백에서 처리? 프론트에서 처리?) -> 위에서 where 절이 빠질지도?
-//	// group별 대학 질문 리스트 조회
-//	@Query("SELECT new com.a304.ggong.dto.response.AllAnswerResponse(q) FROM Question q WHERE q.type = '대학'")
-//	List<AllAnswerResponse> getUniversityQuestions(int questionGroup);
-//
-//	// group별 기업 질문 리스트 조회
-//	@Query("SELECT new com.a304.ggong.dto.response.AllAnswerResponse(q) FROM Question q WHERE q.type = '기업'")
-//	List<AllAnswerResponse> getCompanyQuestions(int questionGroup);
+	//	// group별 대학 질문 리스트 조회
+	//	@Query("SELECT new com.a304.ggong.dto.response.AllAnswerResponse(q) FROM Question q WHERE q.type = '대학'")
+	//	List<AllAnswerResponse> getUniversityQuestions(int questionGroup);
+	//
+	//	// group별 기업 질문 리스트 조회
+	//	@Query("SELECT new com.a304.ggong.dto.response.AllAnswerResponse(q) FROM Question q WHERE q.type = '기업'")
+	//	List<AllAnswerResponse> getCompanyQuestions(int questionGroup);
 
 	// 그 외, 지역별, 연령별, 대학별, 기업별은 프론트에서 질문ID를 넘겨줄 수 있는지의 여부에 따라 메소드가 다르게 짜일듯!
 	// 만약 넘겨준다면?
--- BackEnd/ggong/src/main/java/com/a304/ggong/service/AnswerService.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/service/AnswerService.java
@@ -1,28 +1,32 @@
 package com.a304.ggong.service;
 
-import com.a304.ggong.dto.response.*;
-import org.apache.catalina.LifecycleState;
-
 import java.util.List;
+
+import com.a304.ggong.dto.response.AllAnswerResponse;
+import com.a304.ggong.dto.response.AnswerDetailResponse;
 
 public interface AnswerService {
 
-    // service단에서 각각의 resDto에 answerA와 answerB의 값을 넣어줘야함.
-    // universityName도 마찬가지!
-    // companyName도 마찬가지!
-
-	// 모든 질문 응답 데이터 조회
-    public List<AllAnswerResponse> getAllAnswers(int questionGroup);
+	// service단에서 각각의 resDto에 answerA, answerB와 rateA, rateB의 값을 넣어줘야함.
 
 	// 지역별 질문 응답 데이터 조회
-    public List<AreaAnswerResponse> getAnswersGroupByArea();
+	public List<AnswerDetailResponse> getAnswersGroupByArea();
 
 	// 연령별 질문 응답 데이터 조회
-    public List<AgeAnswerResponse> getAnswersGroupByAge();
+	public List<AnswerDetailResponse> getAnswersGroupByAge();
 
 	// 대학별 질문 응답 데이터 조회
-    public List<UniversityAnswerResponse> getAnswersGroupByUni();
+	public List<AnswerDetailResponse> getAnswersGroupByUni();
 
 	// 회사별 질문 응답 데이터 조회
-    public List<CompanyAnswerResponse> getAnswersGroupByCompany();
+	public List<AnswerDetailResponse> getAnswersGroupByCompany();
+
+	// 공통 질문 응답 데이터 조회
+	public List<AllAnswerResponse> getAnswersGroupByCommon();
+
+	// 대학 질문 응답 데이터 조회
+	public List<AllAnswerResponse> getAnswersGroupByUnis();
+
+	// 기업 질문 응답 데이터 조회
+	public List<AllAnswerResponse> getAnswersGroupByCompanies();
 }
