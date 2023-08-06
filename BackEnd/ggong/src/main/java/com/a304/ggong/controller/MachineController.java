ba3528f3bf430d4780628113145e12308603321b
Jeong Yeji <wjddpwl1222@gmail.com>
2023-08-06 16:45:02 +0900

    Feat: Mod MachineController, Add JwtUtil

--- BackEnd/ggong/src/main/java/com/a304/ggong/controller/MachineController.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/controller/MachineController.java
@@ -1,9 +1,17 @@
 package com.a304.ggong.controller;
 
+import com.a304.ggong.dto.response.AllAnswerResponse;
+import com.a304.ggong.dto.response.AllMachinesResponse;
+import com.a304.ggong.dto.response.LikeResponse;
 import com.a304.ggong.service.MachineService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,4 +19,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class MachineController {
     private final MachineService machineService;
+
+    // 모든 기기 조회
+    @GetMapping
+    public ResponseEntity<List<AllMachinesResponse>> machineList(){
+        List<AllMachinesResponse> machineList = machineService.getAllMachines();
+        return ResponseEntity.status(HttpStatus.OK).body(machineList);
+    }
+
+    // 관심 기기 목록
+    @GetMapping("/like")
+    public ResponseEntity<List<LikeResponse>> likeMachineList(){
+
+    }
+
+    // 특정 기기의 상세 정보 조회
+
+    // 관심 기기 등록
+
+    // 관심 기기 삭제
 }
