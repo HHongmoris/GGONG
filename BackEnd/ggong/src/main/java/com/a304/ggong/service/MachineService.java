f79e91480453c5ebc3f04adc75c3436aa82de323
Jeong Yeji <wjddpwl1222@gmail.com>
2023-08-06 18:21:22 +0900

    Feat: Delete JwtUtil | Mod MachineController, AnswerService, MachineService

--- BackEnd/ggong/src/main/java/com/a304/ggong/service/MachineService.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/service/MachineService.java
@@ -11,19 +11,19 @@ import com.a304.ggong.dto.response.MachineDetailResponse;
 public interface MachineService {
 
 	// 모든 기기 조회
-	List<AllMachinesResponse> getAllMachines();
+	List<AllMachinesResponse> selectAllMachines();
 
 	// 관심 기기 목록 (email로 찾기)
-	List<LikeResponse> getAllFavoriteMachines(String email);
+	List<LikeResponse> selectAllFavoriteMachines(String email);
 
 	// 특정 기기의 상세 정보 조회
-	MachineDetailResponse getMachineDetail(Long machineId);
+	MachineDetailResponse selectMachineDetail(Long machineId);
 
 	// 관심 기기 등록
 	// Response를 프론트에 바로 보내줘버리기...?
-	LikeResponse addFavoriteMachine(String email, LikeRegistRequest entity);
+	void insertFavoriteMachine(String email, LikeRegistRequest entity);
 
 	// 관심 기기 삭제
-	LikeResponse deleteFavoriteMachine(LikeDeleteRequest entity);
+	void deleteFavoriteMachine(LikeDeleteRequest entity);
 
 }
