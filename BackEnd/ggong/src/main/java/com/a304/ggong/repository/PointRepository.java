65e7698ae29e7a903056d3b400783bdd3716c1d6
HHongmoris <moris8470@gmail.com>
2023-08-07 04:45:33 +0900

    Fix: Mod PointRepository

--- BackEnd/ggong/src/main/java/com/a304/ggong/repository/PointRepository.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/repository/PointRepository.java
@@ -2,11 +2,15 @@ package com.a304.ggong.repository;
 
 import com.a304.ggong.entity.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
-    List<Point> findByEventTime(String EventTime);
+    //선택 기간에 따른 포인트 내역 조회
+    @Query("SELECT p FROM Point p WHERE p.eventTime >= :startDate AND p.eventTime <= :endDate")
+    List<Point> findByEventTimeBetween(@Param("startDate") String startDate, @Param("endDate") String endDate);
 }
