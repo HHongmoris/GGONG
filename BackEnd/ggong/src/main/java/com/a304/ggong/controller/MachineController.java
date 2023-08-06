234c0df82b79d183549dc67d21fb9613b5158fb5
Jeong Yeji <wjddpwl1222@gmail.com>
2023-08-07 02:18:07 +0900

    Feat: Add QuestionGroup | Mod LastAnswersController, MachineController, PresentAnswersController, QuestionGroup, MachineService

--- BackEnd/ggong/src/main/java/com/a304/ggong/controller/MachineController.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/controller/MachineController.java
@@ -6,7 +6,10 @@ import com.a304.ggong.dto.response.AllAnswerResponse;
 import com.a304.ggong.dto.response.AllMachinesResponse;
 import com.a304.ggong.dto.response.LikeResponse;
 import com.a304.ggong.dto.response.MachineDetailResponse;
+import com.a304.ggong.exception.FavoriteMachineNotFoundException;
+import com.a304.ggong.global.jwt.service.JwtService;
 import com.a304.ggong.service.MachineService;
+import com.auth0.jwt.interfaces.Claim;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,6 +29,9 @@ public class MachineController {
     @Autowired
     private final MachineService machineService;
 
+    @Autowired
+    private final JwtService jwtService;
+
     // 모든 기기 조회
     @GetMapping
     public ResponseEntity<List<AllMachinesResponse>> getMachineList(){
@@ -32,15 +39,28 @@ public class MachineController {
         return new ResponseEntity<>(machineList, HttpStatus.OK);
     }
 
-//    // 관심 기기 목록
-//    @GetMapping("/like")
-//    public ResponseEntity<List<LikeResponse>> likeMachineList(@RequestHeader(required = true)String token){
-        //try {
-        //			return new ResponseEntity<Article>(articleService.getArticle(articleSeq), HttpStatus.OK);
-        //		} catch (Exception e) {
-        //			throw new ArticleNotFoundException(articleSeq + "번 게시글은 없습니다.");
-        //		}
-//    }
+    // 관심 기기 목록
+    @GetMapping("/like")
+    public ResponseEntity<Object> likeMachineList(@RequestHeader(required = true)String token){
+
+        // token에서 email 빼오기
+        Optional<String> opEmail = jwtService.extractEmail(token);
+
+        if(opEmail.isEmpty()){ // optional Email이 null이라면 토큰이 유효하지 않다는 소리
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        String email = opEmail.get();
+
+        List<LikeResponse> likeList = machineService.selectAllFavoriteMachines(email);
+
+        if (likeList == null) {
+            FavoriteMachineNotFoundException favoriteMachineNotFoundException = new FavoriteMachineNotFoundException();
+            return new ResponseEntity<>(favoriteMachineNotFoundException, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<Object>(likeList, HttpStatus.OK);
+    }
 
     // 특정 기기의 상세 정보 조회
     @GetMapping("/{machineId}")
@@ -53,7 +73,14 @@ public class MachineController {
     @PostMapping
     public ResponseEntity<List<LikeResponse>> registLikeMachine(@RequestHeader String token, LikeRegistRequest request){
         // token에서 email 빼오기
-        String email = null;
+        Optional<String> opEmail = jwtService.extractEmail(token);
+
+        if(opEmail.isEmpty()){ // optional Email이 null이라면 토큰이 유효하지 않다는 소리
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        // 토큰이 유효하다면? email 추출
+        String email = opEmail.get();
         machineService.insertFavoriteMachine(email, request);
 
         // 관심 기기 새로 업데이트 해서 프론트에 주기
@@ -66,8 +93,13 @@ public class MachineController {
     public ResponseEntity<List<LikeResponse>> deleteLikeMachine(@RequestHeader String token, LikeDeleteRequest request){
         machineService.deleteFavoriteMachine(request);
 
+        Optional<String> opEmail = jwtService.extractEmail(token);
+        if(opEmail.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
         // token에서 email 빼오기
-        String email = null;
+        String email = opEmail.get();
         // 관심 기기 새로 업데이트 해서 프론트에 주기
         List<LikeResponse> likes = machineService.selectAllFavoriteMachines(email);
         return new ResponseEntity<>(likes, HttpStatus.OK);
