2855a836791f19ae1a67bc9d35d602ea9f7e1af5
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-07 16:22:02 +0900

    Style: change style related to menubar

--- FrontEnd/ggong/src/components/Button/IconButton.jsx
+++ FrontEnd/ggong/src/components/Button/IconButton.jsx
@@ -29,7 +29,10 @@ const IconButton = ({
 
   return (
     // 토글 버튼이면서 클릭됐으면 toggleIcon을 출력, 그 외에는 icon을 출력
-    <button className={`btn btn-circle ${size && 'btn-sm'} bg-yellow-400 text-zinc-600 `} onClick={handleClick}>
+    <button
+      className={`btn btn-circle ${size && 'btn-sm'} bg-yellow-400 text-zinc-600 border-none`}
+      onClick={handleClick}
+    >
       {toggle && clicked ? toggleIcon : icon}
     </button>
   );
