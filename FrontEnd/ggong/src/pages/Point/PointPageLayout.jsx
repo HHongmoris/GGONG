d93df8424b55df3d9cf9d1254803ffe9d7f35902
정의석 <euiseok96@gmail.com>
2023-08-07 13:50:34 +0900

    Feat: Use useDispatch for rendering

--- FrontEnd/ggong/src/pages/Point/PointPageLayout.jsx
+++ FrontEnd/ggong/src/pages/Point/PointPageLayout.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ProfileCard from '../../components/Card/ProfileCard';
 import { Outlet } from 'react-router-dom';
-
-import { useSelector } from 'react-redux';
+import useApi from '../../hooks/useApi';
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from '../../global/store'; // import 로그인 액션
 
 // 포인트 관련 페이지에 공통으로 적용되는 레이아웃
 const PointPageLayout = () => {
   const { userRating, nickname, points } = useSelector(state => state.user);
+  const dispatch = useDispatch(); // 디스패치 함수 가져오기
+
+  // points 값이 변경될 때마다 실행되는 useEffect
+  useEffect(() => {
+    // API 호출하여 points 값을 업데이트
+    useApi('/users/1', 'GET')
+      .then(res => {
+        // newPoints값 받아오기
+        const newPoints = res.data[0].points;
+        // nickname, userRating도 받아오기
+        const nickname = res.data[0].nickname;
+        const userRating = res.data[0].userRating;
+
+        // Redux 스토어의 points 값을 업데이트, 나머지는 그대로 적용
+        dispatch(login({ nickname, userRating, points: newPoints }));
+      })
+      .catch(error => {
+        console.error('API 호출 오류:', error);
+      });
+  }, [dispatch]); // dispatch 함수가 변경될 때마다 실행
 
   return (
     <div>
