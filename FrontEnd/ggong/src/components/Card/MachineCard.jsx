1a36058f7ab771b79a58ead6f5101bbb5a605a9a
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-07 13:56:49 +0900

    Feat: machine card visible iff machineNo is selected

--- FrontEnd/ggong/src/components/Card/MachineCard.jsx
+++ FrontEnd/ggong/src/components/Card/MachineCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import icons from '../../global/icons';
 
@@ -8,7 +8,7 @@ import LineChart from '../Chart/LineChart';
 import BarChart from '../Chart/BarChart';
 import useApi from '../../hooks/useApi';
 
-const MachineCard = ({ machine = {} }) => {
+const MachineCard = ({ selected, setSelected, machines = [], options = [] }) => {
   const [toggle, setToggle] = useState(true);
 
   // onClick 이벤트 함수 만들어서 넣어주면 될 듯!
@@ -16,19 +16,20 @@ const MachineCard = ({ machine = {} }) => {
   const likeFunction = toggle => {
     if (toggle) {
       // console.log(`1: ${toggle}`);
-      useApi('/machines', 'DELETE').then();
+      useApi(`/machines/${selected}`, 'DELETE').then();
       console.log('DELETE 성공');
     } else {
       // console.log(`2: ${toggle}`);
-      useApi('/machines', 'POST').then();
+      useApi(`/machines/${selected}`, 'POST').then();
       console.log('POST 성공');
     }
   };
+
   return (
     <div className="card border border-black bg-white">
       <div className="card-body">
         <div className="flex justify-between">
-          <Select />
+          <Select options={options} selected={selected} setSelected={setSelected} />
           <IconButton
             icon={icons.HEART}
             toggle={true}
@@ -39,11 +40,19 @@ const MachineCard = ({ machine = {} }) => {
             }}
           />
         </div>
-        <div>
-          {/* <LineChart /> */}
-          <div className="divider"></div>
-          {/* <BarChart title="현재 진행중인 투표" /> */}
-        </div>
+        {/* 기기의 정보를 통해 차트를 그립니다. 선택된 기기번호와 같은 기기의 카드만 화면에 보입니다. */}
+        {machines.map((machine, idx) => {
+          const { machineName, machineNo, userCount } = machine;
+          return (
+            machineNo == selected && (
+              <div key={idx}>
+                <LineChart machineName={machineName} userCount={userCount} />
+                <div className="divider"></div>
+                {/* <BarChart title="현재 진행중인 투표" /> */}
+              </div>
+            )
+          );
+        })}
       </div>
     </div>
   );
