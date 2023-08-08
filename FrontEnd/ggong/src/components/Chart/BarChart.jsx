8e82dd6d078ff2f4d159d83546818cb282b097d1
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-08 13:55:52 +0900

    Fix: fix render bug

--- FrontEnd/ggong/src/components/Chart/BarChart.jsx
+++ FrontEnd/ggong/src/components/Chart/BarChart.jsx
@@ -15,33 +15,26 @@ const BarChart = ({ title, data }) => {
   // 데이터를 구조분해할당 하여 변수 A, B에 저장합니다
   const [A, B] = data;
   const textColor = Text['WHITE'];
-  const colorA = Color['BLUE'];
-  const colorB = Color['RED'];
-
-  const bgA = `bg-${colorA}`;
-  const bgB = `bg-${colorB}`;
-  const gradFrom = `from-${Color['BLUE']}`;
-  const gradTo = `to-${Color['RED']}`;
 
   return (
     <div>
       {/* 차트 제목 */}
       <Subtitle content={title} spacing={true} />
-      <div className={`flex ${textColor} space-x-[-1px]`}>
+      <div className={`flex ${textColor} space-x-[-1px] text-center`}>
         {/* 답변 A에 해당하는 데이터가 표시되는 영역 */}
         <div
           style={{ width: A.ratio + '%' }}
-          className={`flex justify-between items-center min-w-fit ${bgA} h-10 px-2 space-x-1`}
+          className={'flex justify-between items-center min-w-fit bg-blue-400 h-10 px-2 space-x-1'}
         >
           <div>{A.label}</div>
           <div>{A.value}</div>
         </div>
         {/* 답변 A, B를 나누는 영역이며 중간에 VS 문구가 있음 */}
-        <div className={`flex items-center min-w-fit z-10 bg-gradient-to-r ${gradFrom} ${gradTo} h-10 text-xl`}>VS</div>
+        <div className={'flex items-center z-10 bg-gradient-to-r from-blue-400 to-red-400 h-10 text-xl'}>VS</div>
         {/* 답변 B에 해당하는 데이터가 표시되는 영역 */}
         <div
           style={{ width: B.ratio + '%' }}
-          className={`flex justify-between items-center min-w-fit ${bgB} h-10 px-2 space-x-1`}
+          className={'flex justify-between items-center min-w-fit bg-red-400 h-10 px-2 space-x-1'}
         >
           <div>{B.value}</div>
           <div>{B.label}</div>
