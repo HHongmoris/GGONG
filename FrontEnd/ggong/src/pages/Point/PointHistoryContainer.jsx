4499ff78db0f496eb9b601c616a3c4e66bf0d918
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-08 16:47:48 +0900

    Feat: Add Date Input control & search function

--- FrontEnd/ggong/src/pages/Point/PointHistoryContainer.jsx
+++ FrontEnd/ggong/src/pages/Point/PointHistoryContainer.jsx
@@ -7,22 +7,26 @@ import useApi from '../../hooks/useApi';
 
 // 포인트 내역 페이지에 필요한 데이터를 관리하는 컨테이너
 const PointContainer = () => {
+  // 포인트 내역 데이터
   const [points, setPoints] = useState([]);
 
   // location 객체 생성 (현재 URL 정보 가져오기)
   const location = useLocation();
 
-  console.log(location);
-
-  useEffect(() => {
+  const search = (start, end) => {
     useApi('/points/', 'GET').then(res => {
       setPoints(res.data);
+      console.log(start, end);
     });
+  };
+
+  useEffect(() => {
+    search();
   }, [location]);
 
   return (
     <div>
-      <PointHistoryPage data={points} />
+      <PointHistoryPage data={points} search={search} />
     </div>
   );
 };
