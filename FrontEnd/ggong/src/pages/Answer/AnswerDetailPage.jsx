7fecc0e03feae39764b0bb9bf7356d3a3b895842
정의석 <euiseok96@gmail.com>
2023-08-09 13:07:17 +0900

    Feat: get detailData

--- FrontEnd/ggong/src/pages/Answer/AnswerDetailPage.jsx
+++ FrontEnd/ggong/src/pages/Answer/AnswerDetailPage.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Border } from '../../global/colors';
 import BarChart from '../../components/Chart/BarChart';
 import { Tab, category } from '../../components/Tab';
+import useApi from '../../hooks/useApi';
+import { useLocation } from 'react-router-dom';
 
 // 샘플 데이터들... 나중에 데이터 받으면 없앱시다.
 const sampleData1 = [
@@ -22,6 +24,17 @@ const sampleData1 = [
 const AnswerDetailPage = ({ num = 1 }) => {
   // 탭 활성화 관리 (처음 상태 활성화 탭은 첫번째 탭)
   const [activeTab, setActiveTab] = useState(0);
+  const [detailData, setDetailData] = useState([[]]);
+
+  console.log(detailData);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    useApi('/vote/detail', 'GET').then(res => {
+      setDetailData(res.data);
+    });
+  }, [location]);
 
   // 클릭했을 때 tabIndex에 해당하는 탭이 활성화
   const handleTabClick = tabIndex => {
@@ -36,12 +49,25 @@ const AnswerDetailPage = ({ num = 1 }) => {
         </div>
       </div>
       <Tab category={category[num]} activeTab={activeTab} onClick={handleTabClick} />
-      {activeTab === 0 && <BarChart title="대장님 집" data={sampleData1} />}
-      {activeTab === 0 && <BarChart title="홍박사 집" data={sampleData1} />}
-      {activeTab === 1 && <BarChart title="20대" data={sampleData1} />}
-      {activeTab === 1 && <BarChart title="30대" data={sampleData1} />}
-      {activeTab === 2 && <BarChart title="하버드" data={sampleData1} />}
-      {activeTab === 2 && <BarChart title="예일" data={sampleData1} />}
+      {detailData[activeTab].map((data, idx) => {
+        // data들을 다 쪼개서 받기
+        const { labelA, labelB, ratioA, ratioB, valueA, valueB, title } = data;
+        const votes = [
+          // A, B로 구조분해 할당
+          { label: labelA, value: valueA, ratio: ratioA },
+          { label: labelB, value: valueB, ratio: ratioB },
+        ];
+
+        return (
+          <div key={idx}>
+            <div className={`card border ${Border.MAIN} mt-4`}>
+              <div className="p-4">
+                <BarChart title={title} data={votes} />
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
