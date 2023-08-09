bbf92a9b264b1853840ca6bc662a1066e066bbdf
HHongmoris <moris8470@gmail.com>
2023-08-09 10:00:36 +0900

    Fix: Mod API Error

--- BackEnd/ggong/src/main/java/com/a304/ggong/dto/response/PointListResponse.java
+++ BackEnd/ggong/src/main/java/com/a304/ggong/dto/response/PointListResponse.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.Timestamp;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PointListResponse {
-    private String eventTime;
+    private Timestamp eventTime;
     private int balancePoint;
     private int point;
     private String machineName;
