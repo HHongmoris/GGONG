20fdd968069ebaa4ad84b69b0d5c9ab554290148
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-10 17:55:10 +0900

    Fix: Change call api

--- FrontEnd/ggong/src/pages/Login/AuthProcess.jsx
+++ FrontEnd/ggong/src/pages/Login/AuthProcess.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useSearchParams, useNavigate } from 'react-router-dom';
+import { login } from '../../global/store';
 
 const AuthProcess = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -18,7 +19,7 @@ const AuthProcess = () => {
 
     jwt &&
       axios({
-        url: 'http://localhost:8080/api/users/jwt-test',
+        url: 'http://localhost:8080/api/users',
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -26,9 +27,10 @@ const AuthProcess = () => {
         },
       })
         .then(res => {
-          console.log(res);
+          res.data.token = jwt;
+          console.log(res.data);
+          dispatch(login(res.data));
           // dispatch(login(response));
-
           navigate('/');
         })
         .catch(err => console.log(err));
