e2d0bcd58ad661a110b661ecf8789d4d3d1ad2dc
bminlvcoffee <bminlvcoffee@gmail.com>
2023-08-10 17:54:43 +0900

    Feat: Change store to persist container

--- FrontEnd/ggong/src/index.js
+++ FrontEnd/ggong/src/index.js
@@ -8,16 +8,22 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './global/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+export const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+    </PersistGate>
+  </Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
