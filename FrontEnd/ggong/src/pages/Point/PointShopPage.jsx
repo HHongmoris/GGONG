44713662469fbbde5af660c1fbbd29b7e93026f9
정의석 <euiseok96@gmail.com>
2023-08-11 16:03:14 +0900

    Comment: Comment out products data

--- FrontEnd/ggong/src/pages/Point/PointShopPage.jsx
+++ FrontEnd/ggong/src/pages/Point/PointShopPage.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ProductList from '../../components/List/ProductList';
 
-// 상품 종류
-const products = [
-  {
-    price: 5000,
-  },
-  {
-    price: 10000,
-  },
-  {
-    price: 50000,
-  },
-];
+// // 상품 종류
+// const products = [
+//   {
+//     price: 5000,
+//   },
+//   {
+//     price: 10000,
+//   },
+//   {
+//     price: 50000,
+//   },
+// ];
 
 // 포인트샵 페이지
 // 상품 리스트 불러오기
-const PointShopPage = () => {
+const PointShopPage = (products = []) => {
   return (
     <div>
       <ProductList products={products} />
